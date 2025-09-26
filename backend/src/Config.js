240e1d9e74b39a8a208748cb08d1331679289dd4
MonarcaWork <monarcawork8@gmail.com>
2025-09-26 07:35:49 -0400

    cors con la url de netlify

--- backend/src/Config.js
+++ backend/src/Config.js
@@ -5,7 +5,7 @@ const DB_host = process.env.DB_host || "localhost";
 const DB_password = process.env.DB_password || "Codigo123#";
 const DB_database = process.env.DB_database || "treke";
 const DB_port = process.env.DB_port || 5432;
-const DT_hosting = process.env.DT_hosting || "http://localhost:5173";
+const DT_hosting = "https://proyectmanagers.netlify.app" || "http://localhost:4173";
 
 const keyToken = process.env.keyToken || "keytoken103"
 
